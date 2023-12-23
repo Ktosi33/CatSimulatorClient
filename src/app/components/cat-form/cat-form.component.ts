4f0e30411b62c59ceb98f1c99eb6729bc4d7ce2d
Ktosi33 <54289137+Ktosi33@users.noreply.github.com>
2023-12-23 22:53:08 +0100

    feat: better view, images in home

---
+++ src/app/components/cat-form/cat-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cat-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './cat-form.component.html',
+  styleUrl: './cat-form.component.scss'
+})
+export class CatFormComponent {
+
+}
